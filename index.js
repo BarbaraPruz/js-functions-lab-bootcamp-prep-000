bd7fb11d8a6643008f1270fd66d068bcdf72269b
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-02-22 01:39:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ function happyHolidays () {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo (name) {
-  return happyHoldayTo('holidays',name)
-}
 
 function happyHolidayTo (holiday, name) {
   return 'Happy ${holiday}, ${name}!'
 }
+
+function happyHolidaysTo (name) {
+  return happyHoldayTo('holidays',name)
+}
+
 
 function holidayCountdown(holiday, nDays) {
   return "It's ${nDays} days until ${holiday}"
